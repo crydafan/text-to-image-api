f0b681ed464ba8892988d8dc27ab19730d50ddcc
Leonardo <leonardo.estacio.h@gmail.com>
2026-01-01 03:55:22 -0500

    chore: group endpoints into /api/v1

--- src/index.ts
+++ src/index.ts
@@ -3,50 +3,53 @@ import { Elysia, t } from "elysia";
 import OpenAI from "openai";
 
 const app = new Elysia()
-  .get("/api/v1/health", () => ({ success: true, message: "Ok" }))
-  .post(
-    "/api/v1/text-to-image",
-    async ({ body }) => {
-      const { prompt, size } = body;
+  .group("/api/v1", (app) =>
+    app
+      .get("/health", () => ({ success: true, message: "Ok" }))
+      .post(
+        "/text-to-image",
+        async ({ body }) => {
+          const { prompt, size } = body;
 
-      const openai = new OpenAI();
+          const openai = new OpenAI();
 
-      const result = await openai.images.generate({
-        model: "dall-e-3",
-        prompt,
-        size,
-        response_format: "b64_json",
-      });
+          const result = await openai.images.generate({
+            model: "dall-e-3",
+            prompt,
+            size,
+            response_format: "b64_json",
+          });
 
-      const dataBase64 = result.data?.[0].b64_json;
-      const buffer = Buffer.from(dataBase64!, "base64");
+          const dataBase64 = result.data?.[0].b64_json;
+          const buffer = Buffer.from(dataBase64!, "base64");
 
-      const metadata = s3.file(`image_${Date.now()}.png`);
-      await write(metadata, buffer);
+          const metadata = s3.file(`image_${Date.now()}.png`);
+          await write(metadata, buffer);
 
-      const url = metadata.presign({
-        acl: "public-read",
-        expiresIn: 60 * 60, // 1 hour
-      });
+          const url = metadata.presign({
+            acl: "public-read",
+            expiresIn: 60 * 60, // 1 hour
+          });
 
-      return {
-        success: true,
-        message: "Successfully generated image",
-        data: {
-          image_url: url,
+          return {
+            success: true,
+            message: "Successfully generated image",
+            data: {
+              image_url: url,
+            },
+          };
         },
-      };
-    },
-    {
-      body: t.Object({
-        prompt: t.String(),
-        size: t.Union([
-          t.Literal("256x256"),
-          t.Literal("512x512"),
-          t.Literal("1024x1024"),
-        ]),
-      }),
-    }
+        {
+          body: t.Object({
+            prompt: t.String(),
+            size: t.Union([
+              t.Literal("256x256"),
+              t.Literal("512x512"),
+              t.Literal("1024x1024"),
+            ]),
+          }),
+        }
+      )
   )
   .listen(3000);
 
